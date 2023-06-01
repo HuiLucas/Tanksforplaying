b2180d11c37fa227228a1d364961ce6d82cd5ebc
Maur <maurizio2700@gmail.com>
2023-06-01 19:19:40 +0200

    Added some inits, app close on esc key, and a nice app icon

--- main.py
+++ main.py
@@ -1,25 +1,54 @@
-import pygame
-simulation_speed =  1000
+# only allowed to use: pygame, matplotlib, numpy, scipy
+import pygame as pg
+import numpy as np
 
-pygame.init()
-clock = pygame.time.Clock()
-screen = pygame.display.set_mode((100, 100))
+# set up constants
+display_height = 700
+display_width = 1200
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GRAY = (50, 50, 50)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+LIGHT_BLUE = (100, 100, 255)
+LIGHT_RED = (255, 100, 100)
+
+framerate = 1000
+
+
+# pygame init stuff
+
+# screen and clock
+pg.init()
+pg.display.set_caption("Tanks for Playing!")
+clock = pg.time.Clock()
+screen = pg.display.set_mode((display_width, display_height))
+pg.display.set_icon(pg.image.load("Glyphish-Glyphish-23-bird.32.png"))
+
+# font (for writing, obvs)
+pg.font.init()
+font1 = pg.font.SysFont("Comic Sans MS", 45)
+
 
 running = True
 
-while running == True:
-    pygame.event.pump()
+while running:
+    pg.event.pump()
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
             running = False
 
-    screen.fill((0, 0, 255))
+    screen.fill(BLACK)
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
+    keys = pg.key.get_pressed()
+    if keys[pg.K_ESCAPE]:
         running = False
-    pygame.display.flip()
-    clock.tick(simulation_speed)
+    elif keys[pg.K_RIGHT]:
+        screen.blit(font1.render("Right arrow key pressed", False, WHITE), (100, 100))
+    pg.display.flip()
+    clock.tick(framerate)
 
-pygame.quit()
+pg.quit()
