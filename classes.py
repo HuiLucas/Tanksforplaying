b2511286d781cb9c1b9d8ddc1fa4b1a713727a01
Maur <maurizio2700@gmail.com>
2023-06-02 00:11:59 +0200

    Added shooting, gravity.

--- classes.py
+++ classes.py
@@ -2,17 +2,20 @@ import pygame as pg
 import numpy as np
 
 # constants for the classes
-agility_multiplier = 1e-4
+agility_multiplier = 1e-1
 movement_dampening = 1.2
+dt = 0.02  # time step
+gravity = 1e10  # gravity for the bullets. IDK why it has to be so absurdly high (real life G is ~10^-11)
 
-
+# planet class is pretty bare-bones, but it helps a lot with organization
 class Planet:
     def __init__(self, pos, radius, color):
         self.pos = pg.math.Vector2(pos)
         self.radius = radius
         self.color = color
 
-
+# tank class. lots of stuff in here. should probably be renamed to Tank,
+# since I'm planning on including the tank gun in this class as well.
 class TankBody:
     def __init__(self, angle, size, agility, color, boundary_angle):
         self.angle = angle
@@ -26,21 +29,24 @@ class TankBody:
             self.lower_angle_boundary = -4 * np.pi
             self.upper_angle_boundary = 4 * np.pi
         else:
+            # the tank cannot go further than the screen
             self.lower_angle_boundary = - np.pi / 2 - boundary_angle
             self.upper_angle_boundary = - np.pi / 2 + boundary_angle
 
+        # the surface for the tank to be drawn on
         self.surface = pg.Surface(size)
-        self.surface.set_colorkey((0, 0, 0))
+        self.surface.set_colorkey((0, 0, 0))  # do NOT remove this line. IDK what it does exactly, but it all turns
+        # funny if you delete it.
         self.surface.fill(color)
         # you can add an image here of the tank body, just blit an pg.image.load over the surface
 
         # putting these here for other objects to interact with TankBody
         self.x = 0
         self.y = 0
-
+        self.vel = pg.math.Vector2([0,0])
     def move(self, movement=0):
-        self.angvel += self.agility * movement * agility_multiplier
-        self.angle += self.angvel
+        self.angvel += self.agility * movement * agility_multiplier * dt
+        self.angle += self.angvel * dt
 
         # dampen the motion
         self.angvel /= movement_dampening
@@ -56,15 +62,19 @@ class TankBody:
             self.angle = self.lower_angle_boundary
 
     def get_surf(self, planet_pos, planet_radius):
+        # find the tank's x, y coordinates in terms of its angle and stuff
         self.x = (planet_radius + self.size[1] / 4) * np.cos(self.angle) + planet_pos[0]
         self.y = (planet_radius + self.size[1] / 4) * np.sin(self.angle) + planet_pos[1]
+        # useful for others
+        self.vel = self.angvel * planet_radius * pg.math.Vector2([-np.sin(self.angle), np.cos(self.angle)])
 
+        # rotate the tank along with the surface it's on
         rotated_surface = pg.transform.rotate(self.surface, -np.degrees(self.angle))
         rotated_rect = rotated_surface.get_rect(center=(self.x, self.y))
 
         return rotated_surface, rotated_rect
 
-
+# simple class for the crosshair. similar to Planet, it's here only to help with tidiness.
 class Crosshairs:
     def __init__(self, pos, size):
         self.pos = pos
@@ -74,3 +84,33 @@ class Crosshairs:
         self.surface.set_colorkey((0, 0, 0))
         # put the crosshair image
         self.surface.blit(pg.transform.scale(pg.image.load("inverted-crosshair-icon.png"), size), (0, 0))
+        # main.py takes care of where to put the crosshair
+
+# the Bullet class has to deal with gravity and collisions (to be added)
+class Bullet:
+    def __init__(self, pos, size, vel, color):
+        self.pos = pg.math.Vector2(pos)
+        self.size = size
+        self.vel = pg.math.Vector2(vel)
+        self.angle = np.arctan2(self.vel[1], self.vel[0])
+        self.acceleration = pg.math.Vector2([0, 0])
+
+        self.surface = pg.Surface(size)
+        self.surface.set_colorkey((50, 50, 50))
+        self.surface.fill(color)
+
+        self.rotated_surface = pg.transform.rotate(self.surface, -np.degrees(self.angle))
+        self.rotated_rect = self.rotated_surface.get_rect(center=self.pos)
+    def move(self):
+        # these things are similar to the ones above and should be self explanatory
+        self.pos += self.vel * dt
+        self.angle = np.arctan2(self.vel[1], self.vel[0])
+
+        self.rotated_surface = pg.transform.rotate(self.surface, -np.degrees(self.angle))
+        self.rotated_rect = self.rotated_surface.get_rect(center=self.pos)
+
+    def attraction(self, planet_pos):
+        # newton's law of gravitation, pretty simple stuff
+        rel_dist = pg.math.Vector2(planet_pos - self.pos)
+        self.acceleration = gravity * rel_dist / rel_dist.magnitude()**3
+        self.vel += self.acceleration * dt
--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 # only allowed to use: pygame, matplotlib, numpy, scipy
 import pygame as pg
 import numpy as np
+import pygame.mouse
+
 import classes as custom
 
 # set up constants
 display_height = 700
 display_width = 1200
+cool_off_time = 500 # minimum time between shots (in ms)
+bullet_speed = 1200
+
+planet_radius = 4  # these values don't actually mean anything, just fiddle with them until they're okay
+terrain_height = 0.75
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -17,7 +24,6 @@ LIGHT_BLUE = (100, 100, 255)
 LIGHT_RED = (255, 100, 100)
 
 framerate = 60
-dt = 0.01
 
 # pygame init stuff
 
@@ -32,14 +38,26 @@ pg.display.set_icon(pg.image.load("Glyphish-Glyphish-23-bird.32.png"))
 pg.font.init()
 font1 = pg.font.SysFont("Comic Sans MS", 45)
 
+# create the planet
+planet = custom.Planet((display_width / 2, display_height * planet_radius), display_height * (planet_radius - terrain_height), BLUE)
 
-planet = custom.Planet((display_width / 2, display_height * 4.0), display_height * 3.25, BLUE)
+# set up movement boundaries for the tank
 if display_width < planet.radius * 2:
     boundary_angle = np.arcsin((display_width)/(2 * planet.radius))
 else:
     boundary_angle = -10 # -10 is an impossible value, so it's recognizable
-tank = custom.TankBody(-np.pi / 2, (20, 30), 4, RED, boundary_angle)
+
+# create the tank
+tank = custom.TankBody(-np.pi / 2, (20, 30), 8, RED, boundary_angle)
+
+# create the crosshair
 crosshair = custom.Crosshairs((0, 0), (50, 50))
+
+# list containing all the bullets, because we're going to have quite a lot of them
+bullets = [custom.Bullet((100, 100), (40, 10), (1, 1), GREEN)]
+
+# start the cool-off timer
+cooloff_timer = pg.time.get_ticks()
 
 running = True
 
@@ -50,14 +68,28 @@ while running:
         if event.type == pg.QUIT:
             running = False
 
+        # shoot the bullet
+        if pg.mouse.get_pressed()[0]:
+            if pg.time.get_ticks() - cooloff_timer > cool_off_time:
+                # reset the cool off timer
+                cooloff_timer = pg.time.get_ticks()
+
+                # find direction to shoot
+                direction = pg.mouse.get_pos() - pg.math.Vector2(tank.x, tank.y)
+                direction = direction.normalize()
+
+                # create a bullet with init velocity and direction
+                bullets.append(custom.Bullet((tank.x, tank.y), (15, 5), direction * bullet_speed + tank.vel, GREEN))
+
     screen.fill(BLACK)
 
+    # press escape to close the game
     keys = pg.key.get_pressed()
     if keys[pg.K_ESCAPE]:
         running = False
         break
 
-    # move the tank
+    # move the tank (1 is right, -1 is left. don't change these, they act like booleans)
     if keys[pg.K_RIGHT]:
         screen.blit(font1.render("Right arrow key pressed", False, WHITE), (100, 100))
         tank.move(1)
@@ -67,8 +99,8 @@ while running:
     else:
         tank.move(0)
 
-    # text with the tank's angle
-    screen.blit(font1.render(str(tank.angle), False, WHITE), (200, 200))
+    # text with misc content for debugging
+    screen.blit(font1.render(str(pg.time.get_ticks()), False, WHITE), (200, 200))
 
     # display the planet
     pg.draw.circle(screen, planet.color, planet.pos, planet.radius)
@@ -76,6 +108,17 @@ while running:
     # displaying the tank. this needs quite a bit of stuff, so it has to be like this, afaik.
     tank_surface_results = tank.get_surf(planet.pos, planet.radius)
     screen.blit(tank_surface_results[0], tank_surface_results[1])
+
+    # deal with bullets
+    for bullet in bullets:
+        # delete bullets if too far out
+        if bullet.pos[0] > display_width * 2 or bullet.pos[0] < - display_width or bullet.pos[1] > display_height * 2 or bullet.pos[1] < - display_height:
+            del bullet
+        else:
+            # update and display
+            bullet.attraction(planet.pos)
+            bullet.move()
+            screen.blit(bullet.rotated_surface, bullet.rotated_rect)
 
     # display the crosshair
     crosshair.pos = pg.mouse.get_pos()
